daaeab417926f1d650ba0dc8d3d8d5b9dd53a15f
yarden.shacham <yarden101111@gmail.com>
2017-09-27 10:45:20 +0300

    first version build

---
+++ rollup-build.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+const del = require('del');
+const rollup = require('rollup');
+const pkg = require('../package.json');
+const babel = require('rollup-plugin-babel');
+const uglify = require('rollup-plugin-uglify');
+const minify = require('uglify-es').minify;
+
+
+// Clean up the output directory
+// Compile source code into a distributable format with Babel
+// Copy package.json and LICENSE.txt
+Promise.resolve().then(() => del(['dist/*']))
+    .then(() => rollup.rollup({
+        entry: 'src/index.js',
+        external: Object.keys(pkg.dependencies),
+        plugins: [],
+    }).then(bundle => bundle.write({
+        dest: `dist/jx-injector.umd.js`,
+        format: "umd",
+        sourceMap: true,
+        moduleName: pkg.name,
+    })).then(() => rollup.rollup({
+        entry: 'src/index.js',
+        external: Object.keys(pkg.dependencies),
+        plugins: [
+            uglify({}, minify)
+        ],
+    }).then(bundle => bundle.write({
+        dest: `dist/jx-injector.umd.min.js`,
+        format: "umd",
+        sourceMap: true,
+        moduleName: pkg.name,
+    })).then(() => {
+        delete pkg.private;
+        delete pkg.devDependencies;
+        delete pkg.scripts;
+        delete pkg.babel;
+        fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
+        fs.writeFileSync('dist/LICENSE', fs.readFileSync('LICENSE', 'utf-8'), 'utf-8');
+    }).catch(err => console.error(err.stack))));
---
+++ src/injector.ts
@@ -0,0 +1,36 @@
+export class injector {
+    private container: { [key: string]: any }
+    private stack: any
+
+    constructor() {
+        this.container = {};
+    }
+
+    public register(dependencyName: string, dependencyValue: any): void {
+        if (dependencyName && dependencyValue) {
+            this.container[dependencyName] = dependencyValue;
+        }
+    }
+
+    public registerSingleton(dependencyName: string, dependencyValue: any, onDemand?: boolean): void {
+        if (dependencyName && dependencyValue) {
+            if (onDemand) {
+                this.container[dependencyName] = dependencyValue;
+            }
+            else {
+                this.container[dependencyName] = new dependencyValue();
+            }
+        }
+    }
+
+    public get(dependencyName: string) {
+        if (this.container[dependencyName]) {
+            if (typeof  this.container[dependencyName] === "function") {
+                return new this.container[dependencyName];
+            }
+            else
+                return this.container[dependencyName];
+        }
+    }
+}
+

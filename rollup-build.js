f168c8157252489b04c7d4d0896e1d3165013b58
yarde_000 <yarden101111@gmail.com>
2017-09-27 21:49:17 +0300

    fixed rollup bugs

--- rollup-build.js
+++ rollup-build.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const del = require('del');
 const rollup = require('rollup');
 const pkg = require('./package.json');
+const typescript = require('rollup-plugin-typescript');
 const uglify = require('rollup-plugin-uglify');
 const minify = require('uglify-es').minify;
 
@@ -13,30 +14,23 @@ const minify = require('uglify-es').minify;
 // Copy package.json and LICENSE.txt
 Promise.resolve().then(() => del(['dist/*']))
     .then(() => rollup.rollup({
-        entry: 'src/injector',
-        external: Object.keys(pkg.dependencies),
-        plugins: [],
-    }).then(bundle => bundle.write({
-        dest: `dist/jx-injector.umd.js`,
-        format: "umd",
-        sourceMap: true,
-        moduleName: pkg.name,
-    })).then(() => rollup.rollup({
-        entry: 'src/injector',
+        entry: 'src/index.ts',
         external: Object.keys(pkg.dependencies),
         plugins: [
+            typescript(),
             uglify({}, minify)
         ],
     }).then(bundle => bundle.write({
-        dest: `dist/jx-injector.umd.min.js`,
+        dest: `dist/index.js`,
         format: "umd",
         sourceMap: true,
-        moduleName: pkg.name,
-    })).then(() => {
+        moduleName: "jxInjector",
+    }).then(() => {
         delete pkg.private;
         delete pkg.devDependencies;
         delete pkg.scripts;
         delete pkg.babel;
+        fs.writeFileSync('dist/index.ts', fs.readFileSync('src/injector.ts', 'utf-8'), 'utf-8');
         fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
         fs.writeFileSync('dist/LICENSE', fs.readFileSync('LICENSE', 'utf-8'), 'utf-8');
     }).catch(err => console.error(err.stack))));
